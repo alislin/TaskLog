228a86797c96f24d31468bc8c78ebd472be77539
Alis Lin <5921000@qq.com>
2019-11-14 14:54:17 +0800

    add layout

---
+++ TaskLog/TaskLog.Client/Pages/ReportDetail.razor.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TaskLog.Client.Shared;
+using TaskLog.DataModel;
+
+namespace TaskLog.Client.Pages
+{
+    public class ReportDetailBase:TLComponent
+    {
+        protected Report Report { get; set; }
+        [Parameter] public string Key { get; set; }
+    }
+}
